6213b2e91e74e15017fd6bf7c655ce205afdb0ed
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-07-30 15:56:19 -0300

    Life(Tower/Monsters)

--- GAMinG-Jam-2020/Assets/Scripts/CanShoot.cs
+++ GAMinG-Jam-2020/Assets/Scripts/CanShoot.cs
@@ -9,7 +9,17 @@ public class CanShoot : MonoBehaviour
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision != null)
-            canShoot = true;
+        {
+            Life vidaInimigo = collision.GetComponent<Life>();
+            if (vidaInimigo != null)
+            {
+                int aux = vidaInimigo.GetLife();
+                if (aux > 0)
+                    canShoot = true;
+                else
+                    canShoot = false;
+            }
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
---
+++ GAMinG-Jam-2020/Assets/Scripts/Life.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+    [SerializeField] private int vidaMax = 1;
+    [SerializeField] private Sprite[] sprites;
+    [SerializeField] private bool monster = false;
+    private SpriteRenderer spriteRend;
+    private int vidaAtual;
+    private int minimaVida;
+    private int quantSprites;
+
+    private void Awake()
+    {
+        vidaAtual = vidaMax;
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+    private void Start()
+    {
+        if (!monster)
+        {
+            quantSprites = sprites.Length-1;
+            minimaVida = vidaMax / quantSprites;
+        }
+    }
+    private void Update()
+    {
+        if (monster)
+        {
+            if (vidaAtual <= 0)
+                Destroy(this.gameObject);
+
+        }
+        else
+        {
+            if (vidaAtual > 0)
+            {
+                for (int i = 0; i < quantSprites; i++)
+                {
+                    if (vidaAtual >= minimaVida * (i + 1))
+                        spriteRend.sprite = sprites[i];
+                }
+            }
+            else
+            {
+                spriteRend.sprite = sprites[quantSprites];
+            }
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        vidaAtual -= damage;
+        vidaAtual = Mathf.Clamp(vidaAtual, 0, vidaMax);
+    }
+    public void RepareBuild(int cura)
+    {
+        vidaAtual += cura;
+        vidaAtual = Mathf.Clamp(vidaAtual, 0, vidaMax);
+    }
+    public int GetLife()
+    {
+        return vidaAtual;
+    }
+}
